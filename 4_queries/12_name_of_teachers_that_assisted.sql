fa098aa95780a8ac4bf4d0e20a33327cb33a1636
semblant <robynhumphreys7_4@hotmail.com>
2024-12-12 16:56:15 -0700

    Final commit

--- 4_queries/12_name_of_teachers_that_assisted.sql
+++ 4_queries/12_name_of_teachers_that_assisted.sql
@@ -1,10 +1,10 @@
 SELECT
-  teachers.name AS teacher,
+  DISTINCT teachers.name AS teacher,
   cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
 WHERE cohorts.name = 'JUL02'
-GROUP BY teachers.name, cohorts.name
+
 ORDER BY teachers.name
